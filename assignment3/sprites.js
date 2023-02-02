ba37408da85e23467c98dac2d73891d2c4f211b1
jonathan-lag <98864241+jonathan-lag@users.noreply.github.com>
2023-02-02 14:40:45 -0600

    assignment 3 progress

--- assignment3/sprites.js
+++ assignment3/sprites.js
@@ -1,10 +1,84 @@
-let sprite;
+let spriteSheet;
+
+function preload() {
+    spriteSheet = loadImage("assets/SpelunkyGuy.png")
+}
+
 function setup() {
-    createCanvas(600, 600);
-    sprite = createSprite(width/2, height/2, 100, 100);
+    createCanvas(400, 400);
+    imageMode(CENTER);
+
+    walkingAnimation = new WalkingAnimation(spriteSheet, 80, 80, 200, 200, 9);
 }
 
 function draw() {
     background(200);
-    drawSprites();
+    
+    walkingAnimation.draw();
+}
+
+function keyPressed() {
+    walkingAnimation.keyPressed();
+}
+
+function keyReleased() {
+    walkingAnimation.keyReleased();
+}
+
+class WalkingAnimation {
+    constructor(spritesheet, sw, sh, dx, dy, animationLength) {
+        this.spritesheet = spritesheet;
+        this.sw = sw;
+        this.sh = sh;
+        this.dx = dx;
+        this.dy = dy;
+        this.u = 0
+        this.v = 0;
+        this.animationLength = 9;
+        this.currentFrame = 0;
+        this.moving = 0;
+        this.xDirection = 1;
+    }
+
+    draw() {
+        //if (this.moving != 0) {
+        //   this.u = this.currentFrame % this.animationLength;
+        //} else {
+        //    this.u = 0;
+        //}
+        
+        this.u = (this.moving != 0) ? this.currentFrame % this.animationLength :
+        push();
+        translate(this.dx, this.dy);
+        scale(this.xDirection, 1);
+
+        image(this.spritesheet, 0, 0, this.sw, this.sh, this.u*this.sw, this.v*this.sh, this.sw, this.sh);
+        pop();
+    
+
+        if (frameCount % 6 == 0) {
+            this.currentFrame++; 
+        }  
+
+        this.dx += this.moving;
+
+    }
+
+    keyPressed() {
+        if (keyCode === RIGHT_ARROW) {
+            this.moving = 1;
+            this.xDirection = 1;
+            this.currentFrame = 1;
+        } else if (keyCode === LEFT_ARROW) {
+            this.moving = -1;
+            this.xDirection = -1;
+            this.currentFrame = 1;
+        }
+    }
+
+    keyReleased() {
+        if (keyCode === RIGHT_ARROW || keyCode === LEFT_ARROW) {
+            this.moving = 0;
+        }
+    }
 }
